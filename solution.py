dd55456d4b060214ba2ad3570cca649a4140a2fc
MV QUAN <Quan.MV225071@sis.hust.edu.vn>
2026-04-15 15:10:50 +0700

    Submit Lab02/Day10

--- solution.py
+++ solution.py
@@ -64,7 +64,8 @@ def validate(data):
         else:
             error_count += 1
 
-    print(f"Validation complete. Valid: {len(valid_records)}, Errors: {error_count}")
+    #print(f"Validation complete. Valid: {len(valid_records)}, Errors: {error_count}")
+    print(f"Validation complete. Valid: {len(valid_records)}, Error: {error_count}")
     return valid_records
 
 
